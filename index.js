0670c3a49f22789af47fd58b5f35eb6310e55f4a
mdsafiulazam <mdriadkhan20@gmail.com>
2022-04-24 12:46:44 +0600

    used app.delete and remove from server side

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ async function run() {
       const service = await serviceCollection.findOne(query)
       res.send(service)
     })
+    app.post('/service', async(req, res)=>{
+      const serviceDoc = req.body 
+      const result = await serviceCollection.insertOne(serviceDoc)
+      res.send(result)
+    })
+    app.delete('/service/:id', async(req, res)=>{
+      const id = req.params.id 
+      const query = {_id:ObjectId(id)}
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
 
   }
   finally {
